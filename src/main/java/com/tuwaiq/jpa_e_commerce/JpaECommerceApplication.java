860f41d05cd31ccdcdcd3161632d6775f81a9011
osama dad <osamadad768@gmail.com>
2025-11-23 13:11:20 +0300

    initial commit

---
+++ src/main/java/com/tuwaiq/jpa_e_commerce/JpaECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.tuwaiq.jpa_e_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaECommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tuwaiq/jpa_e_commerce/JpaECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuwaiq.jpa_e_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
